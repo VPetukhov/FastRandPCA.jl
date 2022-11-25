fbf37e6315ac1d85c98232daa8945fd2bacc7024
Viktor Petukhov <viktor.petukhov+gh@pm.me>
2022-11-25 16:32:22 +0400

    Basic tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,21 @@
 using FastRandPCA
 using Test
+using SparseArrays
 
 @testset "FastRandPCA.jl" begin
-    # Write your tests here.
+    mat = sprand(100, 20, 0.2)
+
+    eigs = eigSVD(mat)
+    @assert all(size(eigs.vectors) .== size(mat, 2))
+    @assert length(eigs.values) == size(mat, 2)
+    @assert all(size(eigs.projection) .== size(mat))
+
+    k = 10
+    pcs = pca(mat, k; q=3)
+    @assert size(pcs.vectors, 2) == size(mat, 2)
+    @assert size(pcs.vectors, 1) == k
+    @assert length(pcs.values) == k
+    @assert size(pcs.projection, 2) == size(mat, 1)
+    @assert size(pcs.projection, 1) == k
+    @assert all(size(pcs.projection * mat) .== size(pcs.vectors))
 end
