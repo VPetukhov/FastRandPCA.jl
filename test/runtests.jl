4fcea0098781e74c596b78d3f35071f339df03e7
Viktor Petukhov <viktor.petukhov+gh@pm.me>
2022-11-25 20:05:26 +0400

    Fixed tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,19 +3,21 @@ using Test
 using SparseArrays
 
 @testset "FastRandPCA.jl" begin
-    mat = sprand(100, 20, 0.2)
+    for (n,m) in [[100, 20], [20, 100], [1000, 1000]]
+        mat = sprand(n, m, 0.2)
 
-    eigs = eigSVD(mat)
-    @assert all(size(eigs.vectors) .== size(mat, 2))
-    @assert length(eigs.values) == size(mat, 2)
-    @assert all(size(eigs.projection) .== size(mat))
+        eigs = eigSVD(mat)
+        @test all(size(eigs.V) .== size(mat, 2))
+        @test length(eigs.S) == size(mat, 2)
+        @test all(size(eigs.U) .== size(mat))
 
-    k = 10
-    pcs = pca(mat, k; q=3)
-    @assert size(pcs.vectors, 2) == size(mat, 2)
-    @assert size(pcs.vectors, 1) == k
-    @assert length(pcs.values) == k
-    @assert size(pcs.projection, 2) == size(mat, 1)
-    @assert size(pcs.projection, 1) == k
-    @assert all(size(pcs.projection * mat) .== size(pcs.vectors))
+        k = 10
+        pcs = pca(mat, k; q=3)
+        @test size(pcs.Vt, 2) == size(mat, 2)
+        @test size(pcs.Vt, 1) == k
+        @test length(pcs.S) == k
+        @test size(pcs.U, 2) == size(mat, 1)
+        @test size(pcs.U, 1) == k
+        @test all(size(pcs.U * mat) .== size(pcs.Vt))
+    end
 end
